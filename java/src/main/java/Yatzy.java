90318a05be615fc50317f44c9c6d1ca879227ad7
Erwan Lenotte <erwan.lenotte@gmail.com>
2023-11-19 22:33:48 +0100

    refacto (algo) : standardization of the calcul with streams in preparation to factorize it

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,240 +1,172 @@
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+
+    public static final int FIVES_VALUE = 5;
+    public static final int SIXES_VALUE = 6;
+    public static final int ONES_VALUE = 1;
+    private static final int DEFAULT_SCORE = 0;
+    private static final int PAIR_VALUE = 2;
+    private static final int PAIR_TALLIES = 2;
+    private static final int YATSY_SCORE = 50;
+    private static final int YATSY_VALUE = 5;
+    private static final int TRIPLET_VALUE = 3;
+    private static final int TRIPLET_TALLIES = 3;
+    private static final int QUADRUPLET_VALUE = 4;
+    private static final int QUADRUPLET_TALLIES = 4;
+    private static final int NB_DICE_STRAIGHT = 5;
+    private static final int SMALL_STRAIGHT_SCORE = 15;
+    private static final int MAX_DICE_SMALL_STRAIGHT = 5;
+    private static final int LARGE_STRAIGHT_SCORE = 20;
+    private static final int NB_TALLIES_STRAIGHT = 1;
+    private static final int MIN_DICE_LARGE_STRAIGHT = 1;
+    private static final int TWOS_VALUE = 2;
+    private static final int THREES_VALUE = 3;
+    private static final int FOURS_VALUE = 4;
+
+    public static int chance(int d1, int d2, int d3, int d4, int d5) {
+        return Stream.of(d1, d2, d3, d4, d5).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
+    public static int yatzy(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        return tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= YATSY_VALUE)
+            .findAny()
+            .map(entry -> YATSY_SCORE)
+            .orElse(DEFAULT_SCORE);
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == ONES_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == TWOS_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == THREES_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+
+    public static int fours(int d1, int d2, int d3, int d4, int d5) {
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == FOURS_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+    public static int fives(int d1, int d2, int d3, int d4, int d5) {
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == FIVES_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+    public static int sixes(int d1, int d2, int d3, int d4, int d5) {
+        return Stream.of(d1, d2, d3, d4, d5).filter(dice -> dice == SIXES_VALUE).reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+    public static int scorePair(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        return tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= PAIR_TALLIES)
+            .max(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() * PAIR_VALUE)
+            .orElse(DEFAULT_SCORE);
     }
 
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
+    public static int twoPair(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        return tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= PAIR_VALUE)
+            .map(entry -> entry.getKey() * PAIR_VALUE)
+            .reduce(DEFAULT_SCORE, Integer::sum);
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+
+    public static int threeOfAKind(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        return tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= TRIPLET_TALLIES)
+            .max(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() * TRIPLET_VALUE)
+            .orElse(DEFAULT_SCORE);
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+
+    public static int fourOfAKind(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        return tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= QUADRUPLET_TALLIES)
+            .max(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() * QUADRUPLET_VALUE)
+            .orElse(DEFAULT_SCORE);
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        if (tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() == NB_TALLIES_STRAIGHT && entry.getKey() <= MAX_DICE_SMALL_STRAIGHT)
+            .count() == NB_DICE_STRAIGHT)
+            return SMALL_STRAIGHT_SCORE;
+        return DEFAULT_SCORE;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        if (tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() == NB_TALLIES_STRAIGHT && entry.getKey() > MIN_DICE_LARGE_STRAIGHT)
+            .count() == NB_DICE_STRAIGHT)
+            return LARGE_STRAIGHT_SCORE;
+        return DEFAULT_SCORE;
     }
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
+    public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
+
+        Map<Integer, Long> tallies = Stream.of(d1, d2, d3, d4, d5).collect(Collectors.groupingBy(
+            Function.identity(),
+            Collectors.counting()));
+
+        var pairTotal = tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= PAIR_TALLIES)
+            .max(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() * PAIR_VALUE)
+            .orElse(DEFAULT_SCORE);
+
+        var tripletTotal = tallies.entrySet().stream()
+            .filter(entry -> entry.getValue() >= TRIPLET_TALLIES)
+            .max(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() * TRIPLET_VALUE)
+            .orElse(DEFAULT_SCORE);
+
+        if (pairTotal != DEFAULT_SCORE && tripletTotal != DEFAULT_SCORE)
+            return pairTotal + tripletTotal;
+
+        return DEFAULT_SCORE;
     }
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
-    }
 }
 
 
--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -73,98 +73,98 @@ public class YatzyTest {
     @ParameterizedTest
     @CsvFileSource(resources = "test-4s-success.csv", numLinesToSkip = 1)
     public void test_4s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fours());
+        assertEquals(expected,  Yatzy.fours(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("4s : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-4s-fail.csv", numLinesToSkip = 1)
     public void test_4_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fours());
+        assertEquals(expected,  Yatzy.fours(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("5s : score sum of all dice of five")
     @ParameterizedTest
     @CsvFileSource(resources = "test-5s-success.csv", numLinesToSkip = 1)
     public void test_5s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fives());
+        assertEquals(expected, Yatzy.fives(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("5s : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-5s-fail.csv", numLinesToSkip = 1)
     public void test_5_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fives());
+        assertEquals(expected,  Yatzy.fives(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("6s : score sum of all dice of six")
     @ParameterizedTest
     @CsvFileSource(resources = "test-6s-success.csv", numLinesToSkip = 1)
     public void test_6s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).sixes());
+        assertEquals(expected, Yatzy.sixes(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("6s : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-6s-fail.csv", numLinesToSkip = 1)
     public void test_6s_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).sixes());
+        assertEquals(expected,  Yatzy.sixes(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("one_pair : score sum of one pair")
     @ParameterizedTest
     @CsvFileSource(resources = "test-one-pair-success.csv", numLinesToSkip = 1)
     public void test_one_pair_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.score_pair(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.scorePair(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("one_pair : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-one-pair-fail.csv", numLinesToSkip = 1)
     public void test_one_pairs_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.score_pair(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.scorePair(d1, d2, d3, d4, d5));
     }
 
-    @DisplayName("two_pair : score sum of one pair")
+    @DisplayName("two_pair : score sum of two pair")
     @ParameterizedTest
     @CsvFileSource(resources = "test-two-pair-success.csv", numLinesToSkip = 1)
     public void test_two_pair_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.two_pair(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.twoPair(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("two_pair : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-two-pair-fail.csv", numLinesToSkip = 1)
     public void test_two_pairs_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.two_pair(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.twoPair(d1, d2, d3, d4, d5));
     }
 
-    @DisplayName("three_of_a_kind : score sum of one pair")
+    @DisplayName("three_of_a_kind : score sum of a triplet")
     @ParameterizedTest
     @CsvFileSource(resources = "test-three-of-a-kind-success.csv", numLinesToSkip = 1)
     public void test_three_of_a_kind_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.three_of_a_kind(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.threeOfAKind(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("three_of_a_kind : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-three-of-a-kind-fail.csv", numLinesToSkip = 1)
     public void test_three_of_a_kind_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.three_of_a_kind(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.threeOfAKind(d1, d2, d3, d4, d5));
     }
 
-    @DisplayName("four_of_a_kind : score sum of one pair")
+    @DisplayName("four_of_a_kind : score sum of a quadruplet")
     @ParameterizedTest
     @CsvFileSource(resources = "test-four-of-a-kind-success.csv", numLinesToSkip = 1)
     public void test_four_of_a_kind_success(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.four_of_a_kind(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.fourOfAKind(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("four_of_a_kind : score 0")
     @ParameterizedTest
     @CsvFileSource(resources = "test-four-of-a-kind-fail.csv", numLinesToSkip = 1)
     public void test_four_of_a_kind_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
-        assertEquals(expected, Yatzy.four_of_a_kind(d1, d2, d3, d4, d5));
+        assertEquals(expected, Yatzy.fourOfAKind(d1, d2, d3, d4, d5));
     }
 
     @DisplayName("smallStraight : score 15")
@@ -195,7 +195,7 @@ public class YatzyTest {
         assertEquals(expected, Yatzy.largeStraight(d1, d2, d3, d4, d5));
     }
 
-    @DisplayName("fullHouse : score 18")
+    @DisplayName("fullHouse : score total of pair and triplet")
     @ParameterizedTest
     @CsvFileSource(resources = "test-full-house-success.csv", numLinesToSkip = 1)
     public void test_full_house_success(int d1, int d2, int d3, int d4, int d5, int expected) {
