790f988303dbbbe4149087bd18a96bc308689ce7
Erwan Lenotte <erwan.lenotte@gmail.com>
2023-11-19 19:02:05 +0100

    refacto(test): changing acceptance using junit ParameterizedTest and adding fail test case

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -1,133 +1,211 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class YatzyTest {
-    private static final String CHANCES_FilE ="src/test/resources/chance-sum-all-dice.txt";
-    private static List<Integer> CHANCES_EXCEPTED;
 
-    @BeforeAll
-    public static void load_file_result() throws IOException {
-        CHANCES_EXCEPTED = readFileToGetExcepted(CHANCES_FilE);
-    }
-
-    @Test
+    @ParameterizedTest
     @DisplayName("Chance : sum of all dice")
-    public void chance_scores_sum_of_all_dice() {
-        var actual = List.of(Yatzy.chance(2, 3, 4, 5, 1), Yatzy.chance(3,3,4,5,1));
-        assertEquals(CHANCES_EXCEPTED, actual);
+    @CsvFileSource(resources = "chance-sum-all-dice.csv", numLinesToSkip = 1)
+    void chance_scores_sum_of_all_dice(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.chance(d1, d2, d3, d4, d5));
     }
 
-    @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+    @DisplayName("Yatzy : score 50")
+    @ParameterizedTest
+    @CsvFileSource(resources = "yatzy-success.csv", numLinesToSkip = 1)
+    void yatzy_scores_50(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.yatzy(d1, d2, d3, d4, d5));
     }
 
-    @Test public void test_1s() {
-        assertEquals(1, Yatzy.ones(1, 2, 3, 4, 5));
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+    @DisplayName("Yatzy : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "yatzy-fail.csv", numLinesToSkip = 1)
+    void yatzy_scores_0(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.yatzy(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void test_2s() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
+    @DisplayName("1s : score sum of all dice of one")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-1s-success.csv", numLinesToSkip = 1)
+    public void test_1s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.ones(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void test_threes() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
+    @DisplayName("1s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-1s-fail.csv", numLinesToSkip = 1)
+    public void test_1s_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.ones(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void fours_test() 
-    {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+    @DisplayName("2s : score sum of all dice of two")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-2s-success.csv", numLinesToSkip = 1)
+    public void test_2s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.twos(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+    @DisplayName("2s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-2s-fail.csv", numLinesToSkip = 1)
+    public void test_2_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.twos(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+    @DisplayName("3s : score sum of all dice of three")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-3s-success.csv", numLinesToSkip = 1)
+    public void test_3s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.threes(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+    @DisplayName("3s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-3s-fail.csv", numLinesToSkip = 1)
+    public void test_3_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.threes(d1, d2, d3, d4, d5));
     }
 
-    @Test
-    public void two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
+    @DisplayName("4s : score sum of all dice of four")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-4s-success.csv", numLinesToSkip = 1)
+    public void test_4s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fours());
     }
 
-    @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+    @DisplayName("4s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-4s-fail.csv", numLinesToSkip = 1)
+    public void test_4_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fours());
     }
 
-    @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+    @DisplayName("5s : score sum of all dice of five")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-5s-success.csv", numLinesToSkip = 1)
+    public void test_5s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fives());
     }
 
-    @Test
-    public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
+    @DisplayName("5s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-5s-fail.csv", numLinesToSkip = 1)
+    public void test_5_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fives());
     }
 
-    @Test
-    public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
+    @DisplayName("6s : score sum of all dice of six")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-6s-success.csv", numLinesToSkip = 1)
+    public void test_6s_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).sixes());
     }
 
-    @Test
-    public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+    @DisplayName("6s : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-6s-fail.csv", numLinesToSkip = 1)
+    public void test_6s_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).sixes());
     }
 
-
-    private static List<Integer> readFileToGetExcepted(String filePath) throws IOException {
-        return Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8)
-            .stream().map(Integer::parseInt).collect(Collectors.toList());
+    @DisplayName("one_pair : score sum of one pair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-one-pair-success.csv", numLinesToSkip = 1)
+    public void test_one_pair_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.score_pair(d1, d2, d3, d4, d5));
     }
 
+    @DisplayName("one_pair : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-one-pair-fail.csv", numLinesToSkip = 1)
+    public void test_one_pairs_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.score_pair(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("two_pair : score sum of one pair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-two-pair-success.csv", numLinesToSkip = 1)
+    public void test_two_pair_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.two_pair(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("two_pair : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-two-pair-fail.csv", numLinesToSkip = 1)
+    public void test_two_pairs_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.two_pair(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("three_of_a_kind : score sum of one pair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-three-of-a-kind-success.csv", numLinesToSkip = 1)
+    public void test_three_of_a_kind_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.three_of_a_kind(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("three_of_a_kind : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-three-of-a-kind-fail.csv", numLinesToSkip = 1)
+    public void test_three_of_a_kind_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.three_of_a_kind(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("four_of_a_kind : score sum of one pair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-four-of-a-kind-success.csv", numLinesToSkip = 1)
+    public void test_four_of_a_kind_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.four_of_a_kind(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("four_of_a_kind : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-four-of-a-kind-fail.csv", numLinesToSkip = 1)
+    public void test_four_of_a_kind_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.four_of_a_kind(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("smallStraight : score 15")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-small-straight-success.csv", numLinesToSkip = 1)
+    public void test_small_straight_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.smallStraight(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("smallStraight : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-small-straight-fail.csv", numLinesToSkip = 1)
+    public void test_small_straight_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.smallStraight(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("largeStraight : score 15")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-large-straight-success.csv", numLinesToSkip = 1)
+    public void test_large_straight_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.largeStraight(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("largeStraight : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-large-straight-fail.csv", numLinesToSkip = 1)
+    public void test_large_straight_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.largeStraight(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("fullHouse : score 18")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-full-house-success.csv", numLinesToSkip = 1)
+    public void test_full_house_success(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.fullHouse(d1, d2, d3, d4, d5));
+    }
+
+    @DisplayName("fullHouse : score 0")
+    @ParameterizedTest
+    @CsvFileSource(resources = "test-full-house-fail.csv", numLinesToSkip = 1)
+    public void test_full_house_fail(int d1, int d2, int d3, int d4, int d5, int expected) {
+        assertEquals(expected, Yatzy.fullHouse(d1, d2, d3, d4, d5));
+    }
 }
